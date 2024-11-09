87ebca8a2ecc5d996bf10b8d45aad3e8e78d7ca9
ussjwkmwbh <162303617+ussjwkmwbh@users.noreply.github.com>
2024-11-09 13:13:37 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,16 +1,43 @@
-var score = parseInt(document.getElementById('score').innerText);
+var score = parseInt(document.getElementById("score").innerText);
+localStorage.setItem("currentScore", score);
+
+function getHighScore() {
+  let highScore = localStorage.getItem("highScore");
+  document.getElementById('highScore').innerText = `HighScore: ${highScore}`
+  return highScore ? parseInt(highScore) : 0;
+}
+
+function saveHighScore(newScore) {
+  localStorage.setItem("highScore", newScore);
+  document.getElementById('newHighScore').innerText = "New HighScore!";
+}
 
 function appendScore() {
-    score += 1;
-    document.getElementById('score').innerText = score;
-    if (score % 10 === 0) {
-        document.getElementById('alertSound').play();
-    }
-}
+  score += 1;
+  document.getElementById("score").innerText = score;
 
+  if (score % 10 === 0) {
+    document.getElementById("alertSound").play();
+    console.log(score);
+  }
+  localStorage.setItem("currentScore", score);
+  const highScore = getHighScore();
+  if (score > highScore) {
+    saveHighScore(score);
+  }
+}
 
 function clearScore() {
-    document.getElementById('buttonSound').play();
-    score = 0;
-    document.getElementById('score').innerText = score;
+  document.getElementById("buttonSound").play();
+  score = 0;
+  document.getElementById("score").innerText = score;
 }
+
+//Keyboard Controls
+document.addEventListener("keydown", function (event) {
+  if (event.code === "Space") {
+    appendScore();
+  } else if (event.code === "Enter") {
+    clearScore();
+  }
+});
